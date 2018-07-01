231467e994962962ce73243c667cf1f353a8e793
Davide Bologna <dbolog@gnomo>
2018-07-01 15:55:32 +0200

    Testing new register model

---
+++ src/cpu/wide_register.rs
@@ -0,0 +1,68 @@
+#[derive(Debug)]
+pub struct WideReg {
+    h: u8,
+    l: u8,
+}
+
+impl WideReg {
+    /// Returns the 16 bit register value
+    pub fn read() -> u16 {
+
+    }
+
+    /// Writes the 16 bit register value
+    pub fn write(value: u16) {
+
+    }
+
+    /// Most significant bit of the 16 bit register value
+    pub fn msb() -> bool {
+
+    }
+
+    /// Least significant bit of the 16 bit register value
+    pub fn lsb() -> bool {
+
+    }
+
+    /// Returns the 8 bit high register value
+    pub fn read_h() -> u8 {
+
+    }
+
+    /// Writes the 8 bit high register value
+    pub fn write_h(value: u8) {
+
+    }
+
+    /// Most significant bit of the 8bit high register value
+    pub fn msb_h() -> bool {
+
+    }
+
+    /// Least significant bit of the 8bit high register value
+    pub fn lsb_h() -> bool {
+
+    }
+
+    /// Returns the 8 bit low register value
+    pub fn read_l() -> u8 {
+
+    }
+
+    /// Writes the 8 bit low register value
+    pub fn write_l(value: u8) {
+
+    }
+
+    /// Most significant bit of the 8bit low register value
+    pub fn msb_l() -> bool {
+
+    }
+
+    /// Least significant bit of the 8bit low register value
+    pub fn lsb_l() -> bool {
+
+    }
+
+}
